4550647d0d9cb7f4860328f000222c7e5c18c7c2
Vasily Nichiporenko <Vasily Nichiporenko>
2017-10-15 13:08:16 +0300

    Initial upload.

---
+++ hw4/index.php
@@ -0,0 +1,28 @@
+﻿<html>
+<head>
+    <title>Records</title>
+</head>
+<body>
+<?php
+    if(isset($_POST['Name']))
+    {
+        $f = fopen(__DIR__ .'/records','a');
+        fwrite($f,$_POST['Name'].'?*||'.$_POST['Text']."\n");
+        fclose($f);
+    }
+    $lines = file(__DIR__ .'/records');
+    foreach($lines as $rec)
+    {
+        $words = explode('?*||',$rec);
+        echo 'Name: '.$words[0].'<br>';
+        echo 'Recorde: '.$words[1].'<br><br>';
+    }
+?>
+<form action="index.php" method="post"> 
+    <p>Имя: <input type="text" name="Name"><p>
+    <p>Комментарий: <input type="text" name="Text"><p>
+    <input type="submit">
+</form>
+
+</body>
+</html>
